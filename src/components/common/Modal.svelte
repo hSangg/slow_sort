f9aee32fd1fcda790453d8f8a1d56f18ceff9723
Sang <hoai.sang050@gmail.com>
2022-05-10 23:16:44 +0700

    add more features

--- src/components/common/Modal.svelte
+++ src/components/common/Modal.svelte
@@ -1,5 +1,7 @@
 <script>
   let lan = 0;
+  let form;
+  let first = false;
   const isSorted = (a, n) => {
     while (--n >= 1) if (a[n] < a[n - 1]) return false;
 
@@ -16,6 +18,10 @@
     console.log("day la lan xao tron thu: ", ++lan);
   };
 
+  const handleClick = () => {
+    form.reset();
+  };
+
   export const bogoSort = (a, n) => {
     while (!isSorted(a, n)) suffle(a);
   };
@@ -23,20 +29,23 @@
   /////////////////////////////////////////////////////////////////
 
   let button = "sort";
-
   const handelSubmit = (e) => {
     e.preventDefault();
     const data = e.target[0].value.split(",").map((x) => +x);
-    console.log("data: ", data);
+    if (!data[0]) {
+      button = "vui long nhap mang can sort";
+      return;
+    }
+
     bogoSort(data, data.length);
-    console.log("hoan tat:", data);
-    button = `sau ${lan} xao tron thi cung da xong | ${data}`;
+    button = `sau ${lan} xao tron thi cung da xong | ${data[0] ? data : ""}`;
     lan = 0;
+    first = true;
   };
 </script>
 
 <div class="p-2 flex items-center w-full">
-  <form on:submit={handelSubmit} action="" class="w-full p-2">
+  <form bind:this={form} on:submit={handelSubmit} action="" class="w-full p-2">
     <input
       type="text"
       class="w-full  p-2 "
@@ -47,6 +56,13 @@
       type="submit"
       class="w-full bg-green-500 p-2">{button}</button
     >
+    {#if first}
+      <button
+        on:click={handleClick}
+        type="submit"
+        class="w-full bg-green-700 p-2">Thu Lai</button
+      >
+    {/if}
   </form>
 </div>
 
